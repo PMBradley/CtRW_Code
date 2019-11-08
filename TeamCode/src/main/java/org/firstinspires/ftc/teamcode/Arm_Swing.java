51d54371fdb6246c28bb065fd615a4fe3bde7e12
TmoGit <terence@acropolisepc.com>
2019-11-07 20:29:17 -0500

    Outline of MainControl

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm_Swing.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Arm_Swing {
+    double armBaseRotation = 0.0;
+    double wristRotation = 0.0;
+    boolean armStopTriggerOpen = false;
+    boolean armStopTriggerClose = false;
+
+
+
+    public void fullArmRotation(double armSpeed, int armDirection){
+
+        if (armDirection == 1) {
+            //The arm is going to fully open
+        }
+
+        if (armDirection == 0) {
+            //The arm is going to fully closed
+        }
+    }
+
+
+}
