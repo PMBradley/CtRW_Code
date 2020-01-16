6b33287a7b899f940bb2dd8f8c2da65b00051261
FTC-6078 <markortega663@gmail.com>
2020-01-16 16:44:59 -0500

    Merge branch 'master' of C:\Users\FTC-6078\Documents\01 - Project Files\CtRW_Code with conflicts.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive_Meccanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive_Meccanum.java
@@ -4,6 +4,9 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Drive_Meccanum {
 
+
+  //  Robot2019 robot = new Robot2019();
+
     private Robot2019 robot;
 
 
@@ -14,10 +17,10 @@ public class Drive_Meccanum {
 
 
 
-    // Math Variables
+    //Variables
     static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
     static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
-    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     WHEEL_DIAMETER_INCHES   = 3.0 ;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
     static final double     DRIVE_SPEED             = 0.6;
     static final double     TURN_SPEED              = 0.5;
@@ -26,35 +29,60 @@ public class Drive_Meccanum {
 
 
     public void init_motors(){
-        isInitilized = true; // flips dat flag
+        // robot.init(hardwareMap);
+        isInitilized = true;
 
-        robot.driveBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // stops motors and resets encoders
+
+        robot.driveBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.driveFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.driveFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.driveBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    }
 
-    public void Drive_Vector(double xPower, double yPower, double rPower) // drives based off of powers and relative to the field
-    {
 
 
     }
 
-
-    public void drive_Controller(double lStickX, double lStickY, double rStickX) // drives based off of controller inputs and relative to the robot
+    public void Drive_Vector(double x, double y, double r)
     {
+        if(!isInitilized)
+            init_motors();
+
+        double sin = Math.sin(-gyro.getHeading() * 0.0174533);
+        double cos = Math.cos(-gyro.getHeading() * 0.0174533);
+
+        double forward = x * sin + y * cos;
+        double clockwise = r;
+        double right = x * cos - y * sin;
+
+        double fl = forward + clockwise + right;
+        double fr = forward - clockwise - right;
+        double bl = forward + clockwise - right;
+        double br = forward - clockwise + right;
+
+        robot.driveFL.setPower(fl);
+        robot.driveFR.setPower(fr);
+        robot.driveBL.setPower(bl);
+        robot.driveBR.setPower(br);
+    }
+
+    public void drive_Controller(double lStickX, double lStickY, double rStickX)
+    {
+       /* if(!isInitilized)
+            init_motors();
+       */
         double magnitude = Math.hypot(lStickX, lStickY);
         double robotAngle = Math.atan2(lStickY, lStickX)- Math.PI/4;
         double rightX = rStickX;
 
 
-        final double driveFLPwr = magnitude * Math.cos(robotAngle) + rightX; // set each wheel power to the value resulting from the math
+
+        final double driveFLPwr = magnitude * Math.cos(robotAngle) + rightX;
         final double driveFRPwr = magnitude * Math.sin(robotAngle) - rightX;
         final double driveBLPwr = magnitude * Math.sin(robotAngle) + rightX;
         final double driveBRPwr = magnitude * Math.cos(robotAngle) + rightX;
 
-        robot.driveFL.setPower(-driveFLPwr);
-        robot.driveFR.setPower(-driveFRPwr);
+        robot.driveFL.setPower(driveFLPwr);
+        robot.driveFR.setPower(driveFRPwr);
         robot.driveBL.setPower(driveBLPwr);
         robot.driveBR.setPower(driveBRPwr);
     }
