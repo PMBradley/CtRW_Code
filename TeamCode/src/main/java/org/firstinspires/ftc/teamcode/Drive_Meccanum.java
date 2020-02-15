227c3de9de52a7b557914427fe8616db54f5ef6b
FTC-6078 <markortega663@gmail.com>
2020-02-15 14:26:40 -0500

    2/15/2020 -Mark dynamic parking rough finish

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive_Meccanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive_Meccanum.java
@@ -30,6 +30,7 @@ public class Drive_Meccanum {
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
     static final double     DRIVE_SPEED             = 0.6;
     static final double     TURN_SPEED              = 0.5;
+    private double teleTargetHeading = 0;
 
     boolean isInitilized = false;
 
@@ -79,6 +80,51 @@ public class Drive_Meccanum {
         {
             x = x * speedDivisor;
             y = y * speedDivisor;
+        }
+
+        double sin = Math.sin(heading * 0.0174533);
+        double cos = Math.cos(heading * 0.0174533);
+
+        double forward = (x * sin) + (y * cos);
+        double right = (x * cos) - (y * sin);
+
+        robot.fl = forward + (r * turnDivisor) + right;
+        robot.fr = (forward - (r * turnDivisor) - right);
+        robot.bl = forward + (r * turnDivisor) - right;
+        robot.br = (forward - (r * turnDivisor) + right);
+
+        robot.driveFL.setPower(robot.fl);
+        robot.driveFR.setPower(-robot.fr);
+        robot.driveBL.setPower(robot.bl);
+        robot.driveBR.setPower(-robot.br);
+    }
+
+    public void Drive_Gyro_Vector(double x, double y, double r, double heading, boolean limiter, double boostFactor) { // use with auto only
+        //x = Math.sin(heading) * x;
+        //y = Math.cos(heading) * y;
+        teleTargetHeading += r * 5;
+        double minRotPower = 0.32;
+
+        if (boostFactor < .5 && limiter == true)
+        {
+            x = x * speedDivisor;
+            y = y * speedDivisor;
+        }
+
+        if(teleTargetHeading > heading + 180){
+            teleTargetHeading -= 360;
+        }
+        else if(teleTargetHeading < heading - 180){
+            teleTargetHeading += 360;
+        }
+
+        r = r * (teleTargetHeading - heading)/20;
+
+        if(r > 0 && r < minRotPower){
+            r = minRotPower;
+        }
+        else if(r < 0 && r > -minRotPower){
+            r = -minRotPower;
         }
 
         double sin = Math.sin(heading * 0.0174533);
